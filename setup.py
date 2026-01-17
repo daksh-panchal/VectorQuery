2959304f7e81c0ed1a70e3895e70e915c448e838
Daksh Panchal <daksh.panchal@gmail.com>
2026-01-17 14:14:40 -0500

    Added requirements.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="vectorquery",
+    version="0.1.0",
+    author="Daksh Panchal",
+    author_email="daksh.panchal@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
