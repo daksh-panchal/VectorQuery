d0503737272a4090953f4c5f1cdc49cf863662a3
Daksh Panchal <daksh.panchal@gmail.com>
2026-01-17 00:22:22 -0500

    Added folders and files using Bash script.

---
+++ template.sh
@@ -0,0 +1,15 @@
+# Creating directories with mkdir command
+mkdir -p src
+mkdir -p research
+
+# Creating files with the touch command
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb 
+touch requirements.txt
+
+echo "Directories and files created successfully."
