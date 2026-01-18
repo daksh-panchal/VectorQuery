1f632dacbcfd169bd8b4cea438ad488fa01ba592
Daksh Panchal <daksh.panchal@gmail.com>
2026-01-18 17:06:04 -0500

    Updated requirements.txt file.

--- app.py
+++ app.py
@@ -78,10 +78,11 @@ def chat():
 
 
 # This runs the Flask application on localhost port 8080.
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
-
-
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
 
 
 
